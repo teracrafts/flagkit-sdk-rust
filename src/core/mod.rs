c1d630d06f461d0b4b30968cbef58468ddc5f23f
Kingsley Eze <firebox@ikingsley.com>
2026-01-24 21:08:09 +0100

    Update Rust core module exports

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,5 +1,8 @@
 mod cache;
 mod config;
+mod context_manager;
+mod event_queue;
+mod polling_manager;
 
 pub use cache::{Cache, FlagCache};
 pub use config::{
@@ -7,4 +10,15 @@ pub use config::{
     DEFAULT_CIRCUIT_BREAKER_RESET_TIMEOUT, DEFAULT_CIRCUIT_BREAKER_THRESHOLD,
     DEFAULT_EVENT_BATCH_SIZE, DEFAULT_EVENT_FLUSH_INTERVAL, DEFAULT_MAX_CACHE_SIZE,
     DEFAULT_POLLING_INTERVAL, DEFAULT_RETRY_ATTEMPTS, DEFAULT_TIMEOUT,
+};
+pub use context_manager::ContextManager;
+pub use event_queue::{
+    BatchEventsRequest, BatchEventsResponse, Event, EventQueue, EventQueueConfig,
+    EventQueueConfigBuilder, EventSender, DEFAULT_BATCH_SIZE, DEFAULT_FLUSH_INTERVAL_SECS,
+    DEFAULT_MAX_QUEUE_SIZE,
+};
+pub use polling_manager::{
+    PollCallback, PollingConfig, PollingConfigBuilder, PollingManager,
+    DEFAULT_BACKOFF_MULTIPLIER, DEFAULT_JITTER_MS, DEFAULT_MAX_INTERVAL_SECS,
+    DEFAULT_POLLING_INTERVAL_SECS,
 };
